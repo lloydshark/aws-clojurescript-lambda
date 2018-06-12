071b3960263ff0006dd63a3b4d6ac152574b377e
boyd <boyd@lloydshark.net>
2018-06-12 12:04:15 +1000

    Update to figwheel main 0.1.1 snapshot.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
                  [org.clojure/core.async "0.3.442"]
-                 [com.bhauman/figwheel-main "0.1.0"]
+                 [com.bhauman/figwheel-main "0.1.1-SNAPSHOT"]
                  ]
   :plugins [[lein-npm "0.6.2"]
             [lein-cljsbuild "1.1.7"]]
@@ -11,7 +11,7 @@
   :npm {:dependencies [[source-map-support "0.5.3"]
                        [aws-sdk "2.188.0"]]}
   :cljsbuild
-  {:test-commands {"unit-test" ["node" "target/test-main.js"]}
+  {:test-commands {"unit-test" ["node" "target/test-runner.js"]}
    :builds [{:id "lambda-build"
              :source-paths ["src"]
              :compiler {:output-to     "app/main.js"
@@ -23,8 +23,8 @@
                         :optimizations :none}}
             {:id "unit-test"
              :source-paths ["src" "test"]
-             :compiler {:output-to     "target/test-main.js"
-                        :main          "lambda.core-test"
+             :compiler {:output-to     "target/test-runner.js"
+                        :main          "lambda.test-runner"
                         :source-map    true
                         :target        :nodejs
                         :language-in   :ecmascript5
